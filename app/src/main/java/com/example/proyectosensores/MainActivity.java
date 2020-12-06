5c1b3fd3acb18335c31ec2434c4dd9570fb9e420
Juan Bocaranda <juanro151998@gmail.com>
2020-12-06 15:55:43 -0500

    Diseño Página Principal

--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.proyectosensores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        radioGroup = findViewById(R.id.rdgGrupo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (radioGroup.getCheckedRadioButtonId() == -1)
+                {
+                    Toast.makeText(getApplicationContext(), "Seleccione una Opción", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Ha Seleccionado una Opción", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
