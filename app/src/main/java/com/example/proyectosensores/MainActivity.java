b134d50ff8cd3112410060d47b7e05a64e52db24
Juan Bocaranda <juanro151998@gmail.com>
2020-12-07 07:35:56 -0500

    Menú Principal y cambio a todas las Vistas(Corregido)

--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.proyectosensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,33 +13,35 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Button button;
     RadioGroup radioGroup;
-    RadioButton radiobutton;
-    String text;
+    RadioButton radiobutton1;
+    RadioButton radiobutton2;
+    RadioButton radiobutton3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
         radioGroup = findViewById(R.id.rdgGrupo);
+        radiobutton1 = findViewById(R.id.rdbOne);
+        radiobutton2 = findViewById(R.id.rdbTwo);
+        radiobutton3 = findViewById(R.id.rdbThree);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                int selectedID = radioGroup.getCheckedRadioButtonId();
-                radiobutton = findViewById(selectedID);
                 if (radioGroup.getCheckedRadioButtonId() == -1)
                 {
                     Toast.makeText(getApplicationContext(), "Seleccione una Opción", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    if(radiobutton.getText() == "Acelerometro"){
+                    if(radiobutton1.isChecked()){
                         Intent activity = new Intent(getApplicationContext(), Acelerometro.class);
                         startActivity(activity);
                     }
-                    else if(radiobutton.getText() == "Huella"){
-                        Intent activity = new Intent(getApplicationContext(), Huella.class);
+                    else if(radiobutton2.isChecked()){
+                        Intent activity = new Intent(getApplicationContext(), Proximidad.class);
                         startActivity(activity);
                     }
-                    else if(radiobutton.getText() == "GPS"){
+                    else if(radiobutton3.isChecked()){
                         Intent activity = new Intent(getApplicationContext(), Gps.class);
                         startActivity(activity);
                     }
