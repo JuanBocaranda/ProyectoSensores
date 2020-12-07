b11a2feb6cc65d95ec6991123cdd0f69f57d119f
Juan Bocaranda <juanro151998@gmail.com>
2020-12-06 21:16:47 -0500

    Cambio de Actividad

--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.proyectosensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     RadioGroup radioGroup;
+    RadioButton radiobutton;
+    String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +23,17 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         radioGroup = findViewById(R.id.rdgGrupo);
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View view) {
+                int selectedID = radioGroup.getCheckedRadioButtonId();
+                radiobutton = findViewById(selectedID);
                 if (radioGroup.getCheckedRadioButtonId() == -1)
                 {
                     Toast.makeText(getApplicationContext(), "Seleccione una Opción", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    Toast.makeText(getApplicationContext(), "Ha Seleccionado una Opción", Toast.LENGTH_SHORT).show();
+                    Intent acelerometro = new Intent(getApplicationContext(), Acelerometro.class);
+                    startActivity(acelerometro);
                 }
             }
         });
